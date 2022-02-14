5ed3ab244d58f42a226bf86e2cf66e1c70c3616b
IKRAM BENHMIDANE <ikram.benhmidane@gmail.com>
2022-02-14 16:49:55 +0100

    tp12

--- src/main/java/cigma/pfe/controllers/ClientController.java
+++ src/main/java/cigma/pfe/controllers/ClientController.java
@@ -4,30 +4,48 @@ import cigma.pfe.services.ClientService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import cigma.pfe.models.Client;
 
-@Controller("controller1")
+@RestController
+@RequestMapping("/client")
 public class ClientController {
 
 	@Autowired
 	ClientService clientService;
-	
-	public Client save(Client clt) {
+
+	@PostMapping("/create")
+	public Client save(@RequestBody  Client clt) {
 		return clientService.save(clt);
-		}
-		public Client modify(Client clt) {
+	}
+
+	@PutMapping("/update")
+	public Client modify(@RequestBody  Client clt) {
 		return clientService.modify(clt);
-		}
-		public void remove(long idClt) {
-			clientService.remove(idClt);
-		}
-		public Client getOne(long idClt) {
+	}
+
+	public void remove(long idClt) {
+		clientService.remove(idClt);
+	}
+
+	@GetMapping("/{id}")
+	public Client getOne(@PathVariable("id") long idClt) {
 		return clientService.getOne(idClt);
-		}
-		public List<Client> getAll() {
+	}
+
+	@GetMapping("/all")
+	public List<Client> getAll() {
 		return clientService.getAll();
-		}
-		public List<Client> findByName(String name) {
-			return clientService.findByName(name);
-			}
+	}
+
+	public List<Client> findByName(String name) {
+		return clientService.findByName(name);
+	}
 }
