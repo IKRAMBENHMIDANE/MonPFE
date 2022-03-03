68e4928b59f39995e965632e4091c98ac3826234
IKRAM BENHMIDANE <ikram.benhmidane@gmail.com>
2022-03-03 20:48:20 +0100

    TP12

--- src/main/java/cigma/pfe/controllers/ClientController.java
+++ src/main/java/cigma/pfe/controllers/ClientController.java
@@ -4,6 +4,7 @@ import cigma.pfe.services.ClientService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,8 +31,8 @@ public class ClientController {
 	public Client modify(@RequestBody  Client clt) {
 		return clientService.modify(clt);
 	}
-
-	public void remove(long idClt) {
+	@DeleteMapping("/{id}")
+	public void remove(@PathVariable("id") long idClt) {
 		clientService.remove(idClt);
 	}
 
